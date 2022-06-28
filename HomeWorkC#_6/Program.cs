ef9d3702de831d6591e7de621df2f8f4d05b8a8b
Irina <marimashka96@mail.ru>
2022-06-28 21:28:42 +0300

    HW C# 6

--- HomeWorkC#_6/Program.cs
+++ HomeWorkC#_6/Program.cs
@@ -1,4 +1,11 @@
 ﻿
+// Задача 1.
+
+// Проблема при конвертации. Не уверена, что правильно написано. 
+// Программу можно запуститьчерез конвертацию, но высвечивается предупреждение "Разыменование вероятной пустой ссылки". Не понимаю как исправить.
+
+
+/*
 int NumberOfPositiveNumbers (int [] array)
 {
     int counter = 0;
@@ -7,14 +14,24 @@ int NumberOfPositiveNumbers (int [] array)
         if (array[i] > 0) counter++;
     }
     return counter;
+    
 }
+*/
 
+//Проверка программы через конвертацию.
+/*
 Console.Write ("Enter array elements using a spaces: ");
-int[] array = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+int[] myArray = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
 
-int result = NumberOfPositiveNumbers (array);
+int result = NumberOfPositiveNumbers (myArray);
 Console.WriteLine("Number of positive numbers = " + result);
+*/
 
+//Проверка программы через "Магические числа".
+/*
+int [] myArray = new int [] {-1, 2, 3, -7, -9, 0, -100, 1000};
+Console.WriteLine ("Number of positive numbers = " + NumberOfPositiveNumbers(myArray)); 
+*/
 
 
 
@@ -23,19 +40,45 @@ Console.WriteLine("Number of positive numbers = " + result);
 
 
 //Задача 2.
-/*
+// Вопрос: как проверить, что выводится "копированный" массив, а не заданный.
+
 int [] CopyArray (int [] array)
 {
     int [] NewArray = new int[array.Length];
     for (int i = 0; i < array.Length; i++ )
+
     {
-        NewArray [i] = array [i];
+        NewArray [i]  = array [i];
     }
     return NewArray;
+
 } 
 
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++ )
+    {
+        Console.Write (array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+
+// Проверка программы через конвертацию.
+/*
 Console.Write ("Enter array elements using a spaces: ");
 int[] A = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
 
-Console.Write (CopyArray(A));
+PrintArray (A);
+CopyArray (A);
+PrintArray (A);
 */
+
+/* Проверка программы через "Магические числа".
+int [] myArray = {1, -2, 3, 5, -6, 7};
+PrintArray (myArray);
+CopyArray (myArray);
+PrintArray (myArray);
+*/
+
